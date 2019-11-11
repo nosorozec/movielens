a1032e1d837a069115d6024c39733973b2f6e299
ludwik <ludwikowski@gmail.com>
2019-11-11 16:09:20 +0000

    fixed all typos

--- createdb.sh
+++ createdb.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+set +x
 
 DBNAME=ludw.db
 SCHEMA=schema.sql
@@ -8,22 +8,23 @@ SQLITE3=sqlite3
 DATAFILE="ml-latest-small"
 #DATAFILE="ml-20m"
 
-[[ -e $DATAFILE.zip ]] && rm -f $DATAFILE.zip
+[[ -e $DATAFILE.zip ]] && rm -rf $DATAFILE.zip ./$DATAFILE
 [[ -e $DBNAME ]] && rm -f $DBNAME
 
 wget http://files.grouplens.org/datasets/movielens/$DATAFILE.zip
 unzip $DATAFILE.zip
 
-set -i '1d' $DATAFILE/movies.csv
-set -i '1d' $DATAFILE/ratings.csv
+sed -i '1d' $DATAFILE/movies.csv
+sed -i '1d' $DATAFILE/ratings.csv
 
 cat $SCHEMA | $SQLITE3 $DBNAME
 
 cat <<EOF | $SQLITE3 $DBNAME
 .mode csv
 .import $DATAFILE/movies.csv movies
-.import $DATAFILE/ratings.csv movies
+.import $DATAFILE/ratings.csv ratings
 .exit
 EOF
 
-echo "count (*) from table ratings;" | $SQLITE3 $DBNAME
+RAT=`echo "select count(*) from ratings;" | $SQLITE3 $DBNAME`
+echo "Imported: $RAT ratings."
